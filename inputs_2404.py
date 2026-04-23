6fcc102ea29880a995280f5454b3cb8be4cc79a4
helenafrick <helena.frick@student.unisg.ch>
2026-04-23 16:54:15 +0200

    Update inputs_2404.py

--- inputs_2404.py
+++ inputs_2404.py
@@ -17,12 +17,9 @@ st.divider ()
 
 st.subheader("Deine persönlichen Angaben")
 
-st.text_input("Name")
-name = st.text_input("Name")                            #added by H.
-st.number_input("Alter", 0, None, None, 1)
-age = st.number_input("Alter", 0, None, None, 1)        #added by H.
-st.text_input("Studiengang")
-studien = st.text_input("Studiengang")                  #added by H.
+name = st.text_input("Name")                            #modified by H.
+age = st.number_input("Alter", 0, None, None, 1)        #modified by H.
+studien = st.text_input("Studiengang")                  #modified by H.
 
 st.divider ()
 
