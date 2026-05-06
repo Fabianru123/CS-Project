66eb2335d9bb3721d82a74a814b9cc0b3ea2ae18
claiiiireeee <claire.wymann@student.unisg.ch>
2026-05-06 16:20:37 +0200

    Change icon for Lernplan page in navig.py

--- navig.py
+++ navig.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 homepage = st.Page("ordner visual /homepage.py", title="Homepage", icon="🎓")
 profil = st.Page("ordner visual /pages/profil.py", title="Dein Profil", icon="👤")
-lernplan = st.Page("ordner visual /pages/lernplan.py", title="Dein Lernplan", icon="📚")
+lernplan = st.Page("ordner visual /pages/lernplan.py", title="Dein Lernplan", icon="🗓️")
 
 pages = st.navigation([homepage, profil, lernplan], position="sidebar")
 pages.run()
