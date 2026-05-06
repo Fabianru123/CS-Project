05264bb74ae06315bf6a870aff7361be21504365
claiiiireeee <claire.wymann@student.unisg.ch>
2026-05-06 11:54:06 +0200

    Create navig.py

---
+++ navig.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+
+homepage = st.Page("homepage.py", title = "Homepage", icon = "🎓")
+profil = st.Page("pages/profil.py", title = "Dein Profil", icon = "👤")
+lernplan = st.Page("pages/lernplan.py", title = "Dein Lernplan", icon = "📚")
+pages = st.navigation([homepage, profil, lernplan], position = "sidebar")
+pages.run()
