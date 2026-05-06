cfda9e1bf82c92235c522423beda934e948bcce3
claiiiireeee <claire.wymann@student.unisg.ch>
2026-05-06 18:10:33 +0200

    Update navig.py

--- navig.py
+++ navig.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-homepage = st.Page("ordner visual /homepage.py", title="Homepage", icon="🎓")
-profil = st.Page("ordner visual /pages/profil.py", title="Dein Profil", icon="👤")
-lernplan = st.Page("ordner visual /pages/lernplan.py", title="Dein Lernplan", icon="🗓️")
+homepage = st.Page("homepage.py", title="Homepage", icon="🎓")
+profil = st.Page("pages/profil.py", title="Dein Profil", icon="👤")
+lernplan = st.Page("pages/lernplan.py", title="Dein Lernplan", icon="🗓️")
 
 pages = st.navigation([homepage, profil, lernplan], position="sidebar")
 pages.run()
