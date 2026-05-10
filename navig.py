4ab547dae21a9b7264589315068debd4c4cab585
claiiiireeee <claire.wymann@student.unisg.ch>
2026-05-10 12:11:17 +0200

    Update page paths for profil and lernplan

--- navig.py
+++ navig.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 homepage = st.Page("homepage.py", title="Homepage", icon="🎓")
-profil = st.Page("profil.py", title="Dein Profil", icon="👤")
-lernplan = st.Page("lernplan.py", title="Dein Lernplan", icon="🗓️")
+profil = st.Page("pages/profil.py", title="Dein Profil", icon="👤")
+lernplan = st.Page("pages/lernplan.py", title="Dein Lernplan", icon="🗓️")
 
 pages = st.navigation([homepage, profil, lernplan], position="sidebar")
 pages.run()
